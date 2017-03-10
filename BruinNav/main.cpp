e5705ecb35575ad950bdf308b47422d13f6ca582
An An <shangway.hsu@gmail.com>
2017-03-10 13:52:56 -0800

    Initial Commit

---
+++ BruinNav/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BruinNav
+//
+//  Created by Shangyu Hsu on 3/10/17.
+//  Copyright © 2017 Shangyu Hsu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
